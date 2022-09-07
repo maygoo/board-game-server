ec6994dd5a44e4b0f863a043eceee93e5f242e39
maygoo <maygoo@users.noreply.github.com>
2022-09-07 15:42:05 +1000

    removed unnecessary serialisation

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,5 +1,4 @@
 #![warn(clippy::all, rust_2018_idioms)]
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 mod app;
 pub use app::TemplateApp;
@@ -7,15 +6,7 @@ pub use app::TemplateApp;
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    // Log to stdout (if you run with `RUST_LOG=debug`).
-    tracing_subscriber::fmt::init();
-
-    let native_options = eframe::NativeOptions::default();
-    eframe::run_native(
-        "eframe template",
-        native_options,
-        Box::new(|cc| Box::new(TemplateApp::new(cc))),
-    );
+    eprintln!("Complile for wasm");
 }
 
 // when compiling to web using trunk.
